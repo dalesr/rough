faef42eb2e8896e965663cc73bbf58fbeb9dfb4f
dalesr <tomsr@mayfieldj.com>
2018-07-16 11:52:31 -0500

    july 16 updates

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+//import { DeviceOrientation, DeviceOrientationCompassHeading } from '@ionic-native/device-orientation';
 
 @Component({
   selector: 'page-info',
   templateUrl: 'info.html'
 })
 export class InfoPage {
+  lat: any;
+  lng: any;
+  alt: any;
+  spd: any;
+  brg: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public geo: Geolocation) {
 
+  }
+
+  ionViewDidLoad() {
+    this.geo.getCurrentPosition().then(pos => {
+      this.lat = pos.coords.latitude;
+      this.lng = pos.coords.longitude;
+      this.alt = pos.coords.altitude;
+      this.spd = pos.coords.speed;
+      this.brg = pos.coords.heading;
+    }).catch(err => console.log(err));
   }
 
 }
